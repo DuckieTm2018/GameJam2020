335ba3f2a2d43745a6bef20dbd1c28be8a57189a
Tosh-Jompson <giblets2k@gmail.com>
2020-01-31 21:20:10 +0000

    Added: - Dropping - Pickup - Throw - Basic structure for repair interaction

---
+++ GameJam2020/Assets/Josh's Funground/Pickups.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour
+{
+    public GameObject hands;
+    public bool held;
+    public Rigidbody rb;
+    public float throwingPower = 5.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMouseOver()
+    {
+        if (held == false)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                this.gameObject.transform.parent = hands.transform;
+                held = true;
+                rb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (held == true)
+        {
+            transform.localPosition = new Vector3(0f, 0f, 0f);
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                held = false;
+                rb.constraints = RigidbodyConstraints.None;
+            }
+            else if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                held = false;
+                rb.constraints = RigidbodyConstraints.None;
+                rb.AddForce(hands.transform.forward * throwingPower);
+            }
+        }
+    }
+}
---
+++ GameJam2020/Assets/Josh's Funground/RepairPoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairPoint : MonoBehaviour
+{
+    public GameObject partRequired;
+    private Pickups pickups;
+
+    void Start()
+    {
+        pickups = partRequired.GetComponent<Pickups>();
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (pickups.held == true)
+            {
+                pickups.held = false;
+                partRequired.transform.parent = null;
+                partRequired.SetActive(false);
+            }
+        }
+    }
+}
