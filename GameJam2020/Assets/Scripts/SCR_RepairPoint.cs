1d2034f6334b9befe006ad7fb057aa1db38c4702
Pippa <pd161970@falmouth.ac.uk>
2020-02-01 14:37:38 +0000

    Added RepairRoom, which expects the next room that should be repaired after this room. The RepairPoints now require a room and all repair points added to that room need to have manually set unique ids.

--- GameJam2020/Assets/Scripts/SCR_RepairPoint.cs
+++ GameJam2020/Assets/Scripts/SCR_RepairPoint.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class SCR_RepairPoint : MonoBehaviour
 {
+    public int Id;
+    public GameObject room;
     public GameObject partRequired;
     private SCR_Pickups pickups;
+    private SCR_RepairRoom repairRoom;
 
     void Start()
     {
         pickups = partRequired.GetComponent<SCR_Pickups>();
+        repairRoom = room.GetComponent<SCR_RepairRoom>();
+        repairRoom.repairPoints.Add(new KeyValuePair<int, bool>(this.Id, false));
     }
 
     void OnMouseOver()
@@ -21,6 +26,7 @@ public class SCR_RepairPoint : MonoBehaviour
                 pickups.held = false;
                 partRequired.transform.parent = null;
                 partRequired.SetActive(false);
+                repairRoom.UpdateRepairPoint(Id);
             }
         }
     }
---
+++ GameJam2020/Assets/Scripts/SCR_RepairRoom.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Timers;
+
+public class SCR_RepairRoom : MonoBehaviour
+{
+    public string RoomName;
+    public List<KeyValuePair<int, bool>> repairPoints = new List<KeyValuePair<int, bool>>();
+    public float countDownSeconds;
+    public bool isRoomRepaired;
+    public bool startRoom;
+    private static Timer countDown;
+    public GameObject nextRoom;
+    private SCR_RepairRoom nextRepairRoom;
+
+    void Start()
+    {
+        if(nextRoom != null)
+            nextRepairRoom = nextRoom.GetComponent<SCR_RepairRoom>();
+        if (startRoom)
+            Activate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    internal void Activate() 
+    {
+        Debug.Log($"{RoomName}'s timer has started.");
+        countDown = new Timer()
+        {
+            Interval = countDownSeconds * 60,
+            Enabled = true
+        };
+        countDown.Elapsed += TimeRanOut;
+    }
+
+    internal void UpdateRepairPoint(int id)
+    {
+        Debug.Log($"Point {id} for {RoomName} has been repaired.");
+        repairPoints.RemoveAll(r => r.Key == id);
+        repairPoints.Add(new KeyValuePair<int, bool>(id, true));
+
+        if(repairPoints.FindAll(r => r.Value).Count == repairPoints.Count) 
+        {
+            Debug.Log($"Room {RoomName} has been repaired.");
+
+            countDown.Enabled = false;
+            isRoomRepaired = true;
+            if (nextRepairRoom == null) 
+            {
+                Console.WriteLine($"Last room has been repaired.");
+
+                // finished the game!
+            }
+            else
+                nextRepairRoom.Activate();
+        }
+    }
+
+    private void TimeRanOut(object sender, ElapsedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
