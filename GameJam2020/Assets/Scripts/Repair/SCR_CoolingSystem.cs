ddd27666ed8b99ba31834d2f7f96aaaa5deb754d
Pippa <pd161970@falmouth.ac.uk>
2020-02-01 21:46:54 +0000

    Changed the repair system so that units not longer require parts to be repaired.

---
+++ GameJam2020/Assets/Scripts/Repair/SCR_CoolingSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_CoolingSystem : BaseRepairUnit
+{
+    public override bool CanInteract()
+    {
+        return false;
+    }
+
+    public override void Use()
+    {
+
+    }
+
+    void Start()
+    {
+        base.Start();
+        base.requiresParts = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
