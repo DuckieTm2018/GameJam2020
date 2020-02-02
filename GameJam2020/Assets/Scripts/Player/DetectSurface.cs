731f875986d0b1808296de98516509d9b16d085f
JD233113 <JD233113@falmouth.ac.uk>
2020-02-02 15:27:13 +0000

    added hot floor slowdown system + cooling system to door test scene

---
+++ GameJam2020/Assets/Scripts/Player/DetectSurface.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectSurface : MonoBehaviour
+{
+    public bool onHotSurface = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            if (hit.collider.gameObject.name == "SpicyFloor")
+            {
+                onHotSurface = true;
+            }
+            else
+            {
+                onHotSurface = false;
+            }
+        }
+    }
+}
